51a08ac14203b4daf769d5c74e30af16eb6ffd3e
Fazlul Kabir Shohag <shohag.fks@gmail.com>
2021-01-12 15:33:41 +0600

    mysql driver installed

--- src/dbConnection/Connect.java
+++ src/dbConnection/Connect.java
@@ -4,19 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class Connect {
     public static Connection getConnect() {
         try {
-            Class.forName("org.sqlite.JDBC");
-            /*
-            we need a way to make the program read the database when its run from the jar,
-            the resource URL is not getting this job done,
-            otherwise this app is ready to be shipped and wrapped
-            */
-            String UrlInit = "jdbc:sqlite:"; // constant, the rest is the file directory which will be changed on each extract
-            String dbUrl = "Database/PersonDB.db";
-            return DriverManager.getConnection(UrlInit + dbUrl);
+            Class.forName("com.mysql.jdbc.Driver");
+            String database = "attendance";
+            String user = "admin";
+            String password = "attendance@shohag";
+            String port = "1106";
+            String url = "jdbc:mysql://localhost:"+port+"/"+database+"?characterEncoding=latin1";
+            return DriverManager.getConnection(url, user, password);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
