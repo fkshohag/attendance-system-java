a321bd6a6f1f31553f48150a256e8fc5c2b2114d
Fazlul Kabir Shohag <shohag.fks@gmail.com>
2021-01-13 12:56:43 +0600

    usersql dadded

---
+++ migrations/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+	`id` BIGINT unsigned NOT NULL AUTO_INCREMENT,
+	`first_name` VARCHAR(40),
+	`last_name` VARCHAR(40),
+	`email` VARCHAR(70),
+	`password` VARCHAR(100),
+	`role_id` INT,
+	`phone` VARCHAR(40),
+	UNIQUE KEY `users_id` (`id`) USING BTREE,
+	PRIMARY KEY (`id`)
+);
